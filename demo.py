be3ee4209af59e85b91762d56c062641ddd54988
andrewonyango <andrewonyango@users.noreply.github.com>
2016-11-23 13:56:39 +0300

    modified demo to handle exceptions

--- demo.py
+++ demo.py
@@ -1,9 +1,15 @@
 import budget
 
-grants = input("Enter a list of grant requests separated by spaces: ")
-grants = [int(g) for g in grants.split()]
-available_budget = int(input("Enter the available budget: "))
+try:
+    print()  # print a newline
+    grants = input("Enter a list of grant requests separated by spaces: ")
+    grants = [int(g) for g in grants.split()]
+    available_budget = int(input("Enter the available budget: "))
 
-b = budget.Budget()
-
-print("The budget cap is", b.awardBudgetCutProblem(grants, available_budget))
+    b = budget.Budget()
+    print("The budget cap is",
+        b.awardBudgetCutProblem(grants, available_budget))
+except ValueError as error:
+    print(error)
+except:
+    print("An unknown error occured. Check your inputs.")
