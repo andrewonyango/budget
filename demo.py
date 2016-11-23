e3da5f39b47a1cf96104d547abbb69877ba1924f
andrewonyango <andrewonyango@users.noreply.github.com>
2016-11-23 14:02:52 +0300

    incorporated a loop into the demo for better testing

--- demo.py
+++ demo.py
@@ -1,15 +1,17 @@
 import budget
 
-try:
-    print()  # print a newline
-    grants = input("Enter a list of grant requests separated by spaces: ")
-    grants = [int(g) for g in grants.split()]
-    available_budget = int(input("Enter the available budget: "))
+while True:
+    try:
+        print()  # print a newline
+        grants = input("Enter a list of grant requests separated by spaces: ")
+        grants = [int(g) for g in grants.split()]
+        available_budget = int(input("Enter the available budget: "))
 
-    b = budget.Budget()
-    print("The budget cap is",
-        b.awardBudgetCutProblem(grants, available_budget))
-except ValueError as error:
-    print(error)
-except:
-    print("An unknown error occured. Check your inputs.")
+        b = budget.Budget()
+        print("The budget cap is",
+            b.awardBudgetCutProblem(grants, available_budget))
+        break
+    except ValueError as error:
+        print(error)
+    except:
+        print("An unknown error occured. Check your inputs.")
